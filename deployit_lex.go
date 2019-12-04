7349e330e0a31c94dbf5140b4c6ade15ce195d46
leffen <leffen@gmail.com>
2019-12-04 23:05:58 +0100

    0.1.1

--- deployit_lex.go
+++ deployit_lex.go
@@ -6,10 +6,23 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sanity-io/litter"
 )
 
+// VERSION of the application
+const VERSION = "0.1.1"
+
+var (
+	// CommitHash is the revision hash of the build's git repository
+	CommitHash string
+	// BuildTime is the build's time
+	BuildTime string
+)
+
+// Handler handles lex events
 func Handler(ctx context.Context, event events.LexEvent) (*events.LexResponse, error) {
 	fmt.Printf("Received an input from Amazon Lex. Current Intent: %s", event.CurrentIntent.Name)
+	litter.Dump(event)
 
 	messageContent := "Hello from AWS Lambda!"
 
